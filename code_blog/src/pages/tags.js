6fb29104309c95863c28535e6880fc416f5fb918
sai-myne <ahlusir@gmail.com>
2020-09-12 10:29:46 +0800

    Styling & Navbar

---
+++ code_blog/src/pages/tags.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const TagPage = () => (
+  <Layout>
+    <SEO title="Tag" />
+    <h1>Tags page</h1>
+  </Layout>
+)
+
+export default TagPage
---
+++ code_blog/src/pages/team.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const TeamPage = () => (
+  <Layout>
+    <SEO title="Tema" />
+    <h1>Our team....</h1>
+  </Layout>
+)
+
+export default TeamPage
