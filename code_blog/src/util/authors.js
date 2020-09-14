c2223be0d0663ab25080b7577827458c94e31aeb
sai-myne <ahlusir@gmail.com>
2020-09-14 09:04:35 +0800

    Author Detials

---
+++ code_blog/src/util/authors.js
@@ -0,0 +1,24 @@
+const authors = [
+    {
+        name: "John Doe",
+        imageUrl: 'john.jpg',
+        bio: 'John has been a front end and UI designer for over 10 years, he is a brilliant artist',
+        facebook: 'https://www.facebook.com/',
+        facebook: 'https://www.twitter.com/',
+        facebook: 'https://www.instagram.com/',
+        facebook: 'https://www.google.com/',
+        facebook: 'https://www.linkedin.com/',
+    },
+    {
+        name: "Jane Doe",
+        imageUrl: 'jane.jpg',
+        bio: 'John is a back-end developer, she specializes in security and her favorite stack is the MERN stack',
+        facebook: 'https://www.facebook.com/',
+        facebook: 'https://www.twitter.com/',
+        facebook: 'https://www.instagram.com/',
+        facebook: 'https://www.google.com/',
+        facebook: 'https://www.linkedin.com/',
+    }
+]
+
+module.exports = authors;
